a8111bd6eab2cb0d85b5ce3793c2a90626c0d875
chrhodge <chrhodge@cisco.com>
2025-06-17 17:31:00 +0100

    added file formats to config.py

--- run/config.py
+++ run/config.py
@@ -6,9 +6,83 @@ Configuration options for the run module, including file extensions and EXIF tag
 # Determines the minimum number of photos required to create an event folder when restructuring folders.
 EVENT_FOLDER_THRESHOLD = 10
 
-IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.tiff', '.heic', '.bmp', '.png', '.gif']
-RAW_EXTENSIONS = ['.cr2', '.nef', '.arw', '.rw2', '.dng', '.tif']
-VIDEO_EXTENSIONS = ['.mp4', '.mov', '.avi', '.mkv', '.wmv', '.mts', '.m2ts', '.3gp', '.webm']
+IMAGE_EXTENSIONS = [
+    '.jpg', '.jpeg', '.jpe', '.jif', '.jfif', '.jfi',
+    '.png', '.apng',
+    '.tiff', '.tif',
+    '.heic', '.heif',
+    '.bmp', '.dib',
+    '.gif',
+    '.webp',
+    '.pbm', '.pgm', '.ppm', '.pnm',
+    '.svg', '.svgz',
+    '.ico', '.cur',
+    '.emf', '.wmf',
+    '.ras', '.sun', '.im', '.pcx', '.xbm', '.xpm'
+]
+RAW_EXTENSIONS = [
+    '.cr2',  # Canon
+    '.cr3',  # Canon
+    '.nef',  # Nikon
+    '.nrw',  # Nikon
+    '.arw',  # Sony
+    '.srf',  # Sony
+    '.sr2',  # Sony
+    '.rw2',  # Panasonic
+    '.orf',  # Olympus
+    '.pef',  # Pentax
+    '.ptx',  # Pentax
+    '.dng',  # Adobe Digital Negative
+    '.raf',  # Fujifilm
+    '.raw',  # Various
+    '.rwl',  # Leica
+    '.rwz',  # Leica
+    '.k25',  # Kodak
+    '.kdc',  # Kodak
+    '.erf',  # Epson
+    '.mrw',  # Minolta
+    '.mef',  # Mamiya
+    '.mos',  # Leaf
+    '.iiq',  # Phase One
+    '.3fr',  # Hasselblad
+    '.fff',  # Imacon/Hasselblad
+    '.srw',  # Samsung
+    '.bay',  # Casio
+    '.x3f',  # Sigma
+    '.cap',  # Phase One
+    '.iiq',  # Phase One
+    '.mdc',  # Minolta
+    '.tif',  # Some cameras use TIFF as RAW
+    '.heic', # Some modern cameras/phones
+    '.heif'  # Some modern cameras/phones
+]
+VIDEO_EXTENSIONS = [
+    '.mp4', '.mov', '.avi', '.mkv', '.wmv', '.mts', '.m2ts', '.3gp', '.webm',
+    '.flv', '.f4v', '.f4p', '.f4a', '.f4b',  # Flash video
+    '.mpg', '.mpeg', '.mpe', '.mpv', '.mp2', '.m2v',  # MPEG
+    '.ts', '.vob', '.mxf', '.roq', '.nsv',  # Misc
+    '.ogv', '.ogg',  # Ogg
+    '.rm', '.rmvb',  # RealMedia
+    '.asf', '.asx',  # Advanced Streaming Format
+    '.divx',  # DivX
+    '.dv',  # Digital Video
+    '.amv',  # Anime Music Video
+    '.drc',  # Dirac
+    '.mng',  # Multiple-image Network Graphics
+    '.qt',  # QuickTime
+    '.yuv',  # YUV
+    '.svi',  # Samsung video
+    '.3g2',  # 3GPP2
+    '.m4v',  # Apple video
+    '.skm',  # Samsung video
+    '.trp',  # HD video
+    '.vob',  # DVD Video
+    '.wtv',  # Windows Recorded TV Show
+    '.mve',  # Interplay MVE
+    '.ogm',  # OGM
+    '.bik',  # Bink Video
+    '.iso'   # Disc image (sometimes used for video)
+]
 EXIF_TAG_MAP = {
     "ImageWidth": "Exif.Image.ImageWidth",
     "ImageLength": "Exif.Image.ImageLength",
